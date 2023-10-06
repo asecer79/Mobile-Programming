ede9ab197e44dcc90f5f64bf61e1c4c887bf87c7
Aydin Secer <aydinsecer@gmail.com>
2023-10-06 16:11:41 +0300

    u

---
+++ YTU/Dart/w01/02_data_types.dart
@@ -0,0 +1,29 @@
+void main(List<String> args) {
+  //1-Number types
+  //    int
+  //    double
+  //2-String types
+  //3-bool types
+  //4-other object types
+
+  //everything is object
+
+  //number types
+  int age = 21;
+  double salary = 15.5;
+
+  //string
+  String name = "Aydın's";
+  String sname = 'Seçer';
+
+//boolean
+  bool yes = 1 == 1 & 2;
+
+  Object v1 = age;
+
+  Object v2 = "As";
+
+  Object v3 = true;
+
+  print(age);
+}
