2e2f569f59262fd14c554ba395fdafcfa5feebd2
Aydin Secer <aydinsecer@gmail.com>
2023-10-13 15:45:37 +0300

    u

--- YTU/Dart/w02/05_functions_lambda_exp.dart
+++ YTU/Dart/w02/05_functions_lambda_exp.dart
@@ -1,7 +1,11 @@
 void main(List<String> args) {
-  giveMessage1();
-  giveMessage1();
-  giveMessage3("Hi!");
+  //giveMessage1();
+  //giveMessage1();
+  //giveMessage3("Hi!");
+
+  //var message = getMessage4();
+  var message = getMessage5("I am happy!");
+  print(message);
 }
 
 //void methods/functions
@@ -15,4 +19,13 @@ void giveMessage2() {
 
 void giveMessage3(String message) {
   print(message);
+}
+
+//non-void / type returning methods
+String getMessage4() {
+  return "Hello!";
+}
+
+String getMessage5(String message) {
+  return "Your message is: $message";
 }
